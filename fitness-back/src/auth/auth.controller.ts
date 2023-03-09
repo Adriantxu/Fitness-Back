9f869bfac3c77f1dca33b39e4cfa41acb04f449a
Adrian Callejo Calvo <adrian.callejo-calvo@epitech.eu>
2023-03-09 01:02:49 +0100

    Added Update User with validation

--- fitness-back/src/auth/auth.controller.ts
+++ fitness-back/src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthRegisterDto } from './dto/auth.register.dto';
 import { AuthLoginDto } from './dto/auth.login.dto';
@@ -9,6 +15,7 @@ export class AuthController {
 
   // /auth/signup
   @Post('signup')
+  @UsePipes(new ValidationPipe())
   signUp(@Body() dto: AuthRegisterDto) {
     // Body.toString();
     return this.authService.signUp(dto);
@@ -16,6 +23,7 @@ export class AuthController {
 
   // /auth/login
   @Post('login')
+  @UsePipes(new ValidationPipe())
   login(@Body() dto: AuthLoginDto) {
     return this.authService.login(dto);
   }
--- fitness-back/src/user/user.controller.ts
+++ fitness-back/src/user/user.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  Patch,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { UserService } from './user.service';
 import { Utils } from 'src/utils/middlewareHelper';
+import { UserUpdateDto } from './dto';
+import { validate } from 'class-validator';
 
 @Controller('users')
 export class UserController {
@@ -14,6 +27,28 @@ export class UserController {
     const user = this.utils.extractUserJwtMiddleware(req);
     const userId = user ? parseInt(user['sub'] as string) : null;
 
+    if (userId == null)
+      throw new ForbiddenException(
+        'An error happend while retrieving information.',
+      );
     return await this.userService.getUserById(userId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Patch('me')
+  @UsePipes(new ValidationPipe())
+  async updateProfile(@Req() req: Request, @Body() rawBody: UserUpdateDto) {
+    const user = this.utils.extractUserJwtMiddleware(req);
+    const userId = user ? parseInt(user['sub'] as string) : null;
+    const errors = await validate(rawBody);
+    if (errors.length > 0) {
+      throw new BadRequestException(errors);
+    }
+    if (userId == null) {
+      throw new ForbiddenException(
+        'An error happend while retrieving information.',
+      );
+    }
+    return await this.userService.updateProfile(userId, rawBody);
   }
 }
